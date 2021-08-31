b586372a1f6187b72fb59607ab31eff55d69c67e
Amelss <ameleyka@gmail.com>
2021-08-31 15:35:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const enterBtn = document.getElementById("enter-btn");
+const error = document.getElementById("error");
+const email = document.getElementById("email");
+const message = document.querySelector(".message");
+
+enterBtn.addEventListener("click", function () {
+  if (email.validity.typeMismatch) {
+    error.style.visibility = "visible";
+    email.style.borderColor = "red";
+    message.style.color = "red";
+    message.textContent = "Please enter a valid email address";
+  } else {
+    email.value = "";
+    error.style.visibility = "hidden";
+    email.style.borderColor = "hsla(0, 36%, 70%, 40%)";
+    message.style.color = "hsla(0, 36%, 70% ,80%)";
+    message.textContent = "Thank you for your email address";
+  }
+});
+
+email.addEventListener("click", function () {
+  message.textContent = "";
+});
